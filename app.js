765077c4f5c952ec1d9ad876c11c4380a39e799c
thewesker <talorb@me.com>
2016-06-13 19:12:16 -0400

    Change to use environment variables

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var config = require('./config.json');
 var Twitter = require('twitter');
 
 function DeleteTrump(api) {
-  this.client = new Twitter(api);
+  this.client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
 }
 
 DeleteTrump.prototype = {
